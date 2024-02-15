40cd5a96d99fe8be083c32e43de6cd327ab16bdf
limdp99 <dlim@pm-studios.com>
2024-02-15 12:08:57 +0900

    Updates

---
+++ precache-manifest.303f583c5e5f87fdb6a373ce0f9b33fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33835a5552bcce2cdcd078262ad5e915",
+    "url": "/redeem_gg/index.html"
+  },
+  {
+    "revision": "adf240af5da4b9989586",
+    "url": "/redeem_gg/static/css/main.62dc7852.chunk.css"
+  },
+  {
+    "revision": "9c2c797dad8a5e54d79f",
+    "url": "/redeem_gg/static/js/2.70d39452.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/redeem_gg/static/js/2.70d39452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf240af5da4b9989586",
+    "url": "/redeem_gg/static/js/main.4bfa072c.chunk.js"
+  },
+  {
+    "revision": "62c33a66aa565411a4b7",
+    "url": "/redeem_gg/static/js/runtime-main.d2a0d904.js"
+  }
+]);
